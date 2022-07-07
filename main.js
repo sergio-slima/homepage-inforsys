3b31eaedef451d83e448b963de9b22a89333e6c7
Sergio Silva Lima <sergio_silvalima@hotmail.com>
2022-07-07 17:46:47 -0300

    corrigindo navigation

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ function activateMenuAtCurrentSection(section) {
 }
 
 function showNavOnScroll() {
+  const navigation = document.querySelector('#navigation')
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
